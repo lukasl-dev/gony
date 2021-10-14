e7e81f0416592bd55295fa9db795f2fcd600a0e1
lukasl-dev <git@lukasl.dev>
2021-10-14 19:11:11 +0200

    feat: add Connect method and ConnectOptions struct
    Used to open a AnyDesk session.

---
+++ connect.go
@@ -0,0 +1,54 @@
+package granny
+
+// ConnectOptions holds optional configuration options for the Connect method.
+type ConnectOptions struct {
+	RunOptions
+
+	// FileTransfer specifies whether a file transfer session should be started.
+	FileTransfer bool `json:"fileTransfer,omitempty"`
+
+	// FullScreen specifies whether to start the session in fullscreen mode.
+	FullScreen bool `json:"fullScreen,omitempty"`
+
+	// Plain specifies whether to start a session without window title and toolbar.
+	Plain bool `json:"plain,omitempty"`
+}
+
+// defaultConnectOptions are the default ConnectOptions which are used if none
+// are given.
+var defaultConnectOptions = ConnectOptions{
+	RunOptions: defaultRunOptions,
+}
+
+// Connect opens a session to the destination. The destination should be an AnyDesk
+// address, such as 'foo@ad' or '871813768'.
+// See: https://support.anydesk.com/Command_Line_Interface
+func Connect(destination string, opts ...ConnectOptions) error {
+	p := pickConnectOptions(opts...)
+	return Run(buildConnectArgs(destination, p), p.RunOptions)
+}
+
+// pickConnectOptions picks the first index of the passed opts or the defaultConnectOptions
+// if none are given.
+func pickConnectOptions(opts ...ConnectOptions) ConnectOptions {
+	if len(opts) == 0 {
+		return defaultConnectOptions
+	}
+	return opts[0]
+}
+
+// buildConnectArgs builds the cli arguments for the Connect method from the given
+// location and opts.
+func buildConnectArgs(destination string, opts ConnectOptions) (args []string) {
+	args = append(args, destination)
+	if opts.FileTransfer {
+		args = append(args, "--file-transfer")
+	}
+	if opts.FullScreen {
+		args = append(args, "--fullscreen")
+	}
+	if opts.Plain {
+		args = append(args, "--plain")
+	}
+	return args
+}
